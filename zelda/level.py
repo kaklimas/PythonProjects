4ec5eb2d552d11073df46e81e1e999fc88153beb
kacper klimas <kaklimas@student.agh.edu.pl>
2022-02-07 17:26:45 +0100

    deep feelings

--- zelda/level.py
+++ zelda/level.py
@@ -10,8 +10,8 @@ class Level:
         self.display_surface = pygame.display.get_surface()
         
         #sprite group setup
-        self.visible_sprites = pygame.sprite.pygame.sprite.Group()
-        self.obstacles_sprites = pygame.sprite.pygame.sprite.Group()
+        self.visible_sprites = YsortCameraGroup()
+        self.obstacles_sprites = pygame.sprite.Group()
     
         # sprite setup
         self.create_map()
@@ -27,6 +27,24 @@ class Level:
                     self.player = Player((x, y), [self.visible_sprites], self.obstacles_sprites)
     
     def run(self):
-        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
-        debug(self.player.direction)
+        
+        
+class YsortCameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
+        self.offset = pygame.math.Vector2()
+        
+    def custom_draw(self, player):
+        
+        #gettin the offset
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+        for sprite in self.sprites():
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image, offset_pos)
+        
