0ab9ffda5ae86aaf607709149390ad888c772622
kacper klimas <kaklimas@student.agh.edu.pl>
2022-02-07 16:11:16 +0100

    collision detect

--- zelda/level.py
+++ zelda/level.py
@@ -24,7 +24,7 @@ class Level:
                 if col == 'x':
                     Tile((x, y), [self.visible_sprites, self.obstacles_sprites])
                 if col == 'p':
-                    self.player = Player((x, y), [self.visible_sprites])
+                    self.player = Player((x, y), [self.visible_sprites], self.obstacles_sprites)
     
     def run(self):
         self.visible_sprites.draw(self.display_surface)
