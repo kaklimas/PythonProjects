2c38ae329ce5c885c16c972a927f12529cb1d336
kacper klimas <kaklimas@student.agh.edu.pl>
2022-02-07 15:10:43 +0100

    things

--- zelda/level.py
+++ zelda/level.py
@@ -1,5 +1,7 @@
 import pygame
-
+from tile import Tile
+from settings import *
+from player import Player
 class Level:
     def __init__(self):
         #get the display surface
@@ -9,5 +11,20 @@ class Level:
         self.visible_sprites = pygame.sprite.pygame.sprite.Group()
         self.obstacles_sprites = pygame.sprite.pygame.sprite.Group()
     
+        # sprite setup
+        self.create_map()
+        
+    def create_map(self):
+        for row_index, row in enumerate(WORLD_MAP):
+            for col_index, col in enumerate(row):
+                x = col_index * TILESIZE
+                y = row_index * TILESIZE
+                if col == 'x':
+                    Tile((x, y), [self.visible_sprites, self.obstacles_sprites])
+                if col == 'p':
+                    Player((x, y), [self.visible_sprites])
+    
     def run(self):
-        pass
+        self.visible_sprites.draw(self.display_surface)
+    
+    
