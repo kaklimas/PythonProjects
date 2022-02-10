a0d25467729bf3c082e9346751c20e5044e7c683
kaklimas <klimaskacper@interia.pl>
2022-02-10 19:51:45 +0100

    UI

--- zelda/ui.py
+++ zelda/ui.py
@@ -3,7 +3,23 @@ from settings import *
 
 class UI:
     def __init__(self):
-        pass
+        
+        # general
+        self.display_surface = pygame.display.get_surface()
+        self.font = pygame.font.Font(UI_FONT, UI_FONT_SIZE)
+        
+        # bar setup
+        self.health_bar_rect = pygame.Rect(10, 10, HEALTH_BAR_WIDTH,BAR_HEIGHT)
+        self.energy_bar_rect = pygame.Rect(10, 34, ENERGY_BAR_WIDTH, BAR_HEIGHT)
+    
+    def show_bar(self, current, max_amount, bg_rect, color):
+        
+        # draw bg
+        pygame.draw.rect(self.display_surface, UI_BG_COLOR, bg_rect)
+    
+        # draw the bar
     
     def display(self, player):
-        pass
+        self.show_bar(player.health, player.stats['health'], self.health_bar_rect, HEALTH_COLOR)
+        self.show_bar(player.energy, player.stats['energy'], self.energy_bar_rect, ENERGY_COLOR)
+        
