e8beb512b4f61c28e355056c164810b142d3caa1
kaklimas <klimaskacper@interia.pl>
2022-02-13 14:23:30 +0100

    hover attacked enemies

--- zelda/level.py
+++ zelda/level.py
@@ -17,16 +17,21 @@ class Level:
         #sprite group setup
         self.visible_sprites = YsortCameraGroup()
         self.obstacles_sprites = pygame.sprite.Group()
-    
+        
+        self.attack_sprites = pygame.sprite.Group()
+        self.attackable_sprites = pygame.sprite.Group()
+        
         # sprite setup
         self.create_map()
         
         # attack sprites
         self.current_attack = None
         
+        
         # user interface
         self.ui = UI()
-    
+
+        
     def create_map(self):
         layouts = {
             'boundary': import_csv_layout('./map/map_FloorBlocks.csv'),
@@ -50,7 +55,9 @@ class Level:
                             Tile((x, y), [self.obstacles_sprites], 'invisible')
                         if style == 'grass':
                             random_grass_img = choice(graphics['grass'])
-                            Tile((x, y), [self.visible_sprites, self.obstacles_sprites], 'grass', random_grass_img)
+                            Tile((x, y), 
+                                 [self.visible_sprites, self.obstacles_sprites, self.attackable_sprites], 
+                                 'grass', random_grass_img)
                             
                         if style == 'objects':
                             surf = graphics['objects'][int(col)] 
@@ -72,11 +79,12 @@ class Level:
                                     monster_name = 'raccoon'
                                 else:
                                     monster_name = 'squid'    
-                                Enemy(monster_name, (x, y), [self.visible_sprites], self.obstacles_sprites)                        
-                            
-        
+                                Enemy(monster_name, (x, y),
+                                      [self.visible_sprites, self.attackable_sprites], 
+                                      self.obstacles_sprites)                        
+                                 
     def create_attack(self):
-        self.current_attack = Weapon(self.player, [self.visible_sprites])       
+        self.current_attack = Weapon(self.player, [self.visible_sprites, self.attack_sprites])       
     
     def create_magic(self, style, strength, cost):
         print(style)
@@ -86,10 +94,19 @@ class Level:
             self.current_attack.kill()
         self.current_attack = None
     
+    def player_attack_logic(self):
+        if (self.attack_sprites):
+            for attack_sprite in self.attack_sprites:
+                collision_sprites = pygame.sprite.spritecollide(attack_sprite,self.attackable_sprites,True)
+                if collision_sprites:
+                    for target_sprite in collision_sprites:
+                        target_sprite.kill()
+                    
     def run(self):
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
         self.visible_sprites.enemy_update(self.player)
+        self.player_attack_logic()
         self.ui.display(self.player)
         
         
